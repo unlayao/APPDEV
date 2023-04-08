32bf93f127ddbb54ec5a25cb9137f03576cc7858
unlayao <202111092@gordoncollege.edu.ph>
2023-04-08 15:14:06 +0800

    AppDev - Time Bar + Input

--- app/src/main/java/com/example/tasking/MainTask.java
+++ app/src/main/java/com/example/tasking/MainTask.java
@@ -2,11 +2,13 @@ package com.example.tasking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -26,16 +28,22 @@ public class MainTask extends AppCompatActivity {
 
     private long mTimeLeftInMS;
 
+    //progress bar
+    private ProgressBar pb;
+    private int pbMax;
+    private String input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_task);
-        tempTime = (EditText) findViewById(R.id.editTextTime);
+        tempTime = findViewById(R.id.editTextTime);
         //mTextViewCountDown = (TextView) findViewById(R.id.editTextTime);
-        mButtonStartPause = (Button) findViewById(R.id.btnStartTime_PauseTime);
-        mButtonReset = (Button) findViewById(R.id.resetButton);
-
+        mButtonStartPause = findViewById(R.id.btnStartTime_PauseTime);
+        mButtonReset = findViewById(R.id.resetButton);
+        pb = findViewById(R.id.timerBar);
+        tempTime.setText("");
         //Click button will start or pause
         mButtonStartPause.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -44,16 +52,18 @@ public class MainTask extends AppCompatActivity {
                 long temporaryTime = 80000;
                 START_TIME_IN_MS =  temporaryTime;
                 mTimeLeftInMS = START_TIME_IN_MS;*/
-
                 if(mTimerRunning){
                     pauseTimer();
                 } else {
-                    tempTime.setEnabled(false);
-                    String input = tempTime.getText().toString();
+
+                    input = tempTime.getText().toString();
                     long millisInput = Long.parseLong(input) * 60000;
                     setTime(millisInput);
                     startTimer();
                 }
+                tempTime.setVisibility(View.INVISIBLE);
+                pbMax = Integer.parseInt(input) * 60000;
+                pb.setMax(pbMax);
             }
         });
 
@@ -65,7 +75,7 @@ public class MainTask extends AppCompatActivity {
             }
         });
 
-        updateCountDownText();
+        //updateCountDownText();
     }
 
 
@@ -82,7 +92,9 @@ public class MainTask extends AppCompatActivity {
         mTimer = new CountDownTimer(mTimeLeftInMS, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
+                int current = pb.getProgress() + 1000;
                 mTimeLeftInMS = millisUntilFinished;
+                pb.setProgress(current);
                 updateCountDownText();
             }
 
@@ -92,6 +104,8 @@ public class MainTask extends AppCompatActivity {
                 mButtonStartPause.setText("Start");
                 mButtonStartPause.setVisibility(View.INVISIBLE);
                 mButtonReset.setVisibility(View.VISIBLE);
+                tempTime.setVisibility(View.VISIBLE);
+                tempTime.setText("");
             }
         }.start();
 
@@ -112,11 +126,13 @@ public class MainTask extends AppCompatActivity {
     private void resetTimer(){
         tempTime.setEnabled(true);
         tempTime.setText("");
+        pb.setProgress(0);
         mTimeLeftInMS = START_TIME_IN_MS;
-        updateCountDownText();
+        //updateCountDownText();
         mButtonReset.setVisibility(View.INVISIBLE);
         mButtonStartPause.setVisibility(View.VISIBLE);
     }
+
 
     //Updating the timer state
     private void updateCountDownText(){
@@ -124,7 +140,6 @@ public class MainTask extends AppCompatActivity {
         int seconds = (int) (mTimeLeftInMS / 1000) % 60;
 
         String timeLeftFormatted = String.format(Locale.getDefault(),"%02d%02d", minutes, seconds);
-
         tempTime.setText(timeLeftFormatted);
     }
 }

89d3d1dd3e9be61d2827b15cefa98cf317a526ad
63906 <202110256@gordoncollege.edu.ph>
2023-05-10 00:01:44 +0800

    AppDev - with NavBar + Sample Fragments

--- app/src/main/java/com/example/tasking/AboutFragment.java
+++ app/src/main/java/com/example/tasking/AboutFragment.java
@@ -1,6 +1,7 @@
 package com.example.tasking;
 
 import android.os.Bundle;
+import android.sax.Element;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutFragment extends Fragment {
 
@@ -15,5 +17,8 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
+
+
     }
+
 }

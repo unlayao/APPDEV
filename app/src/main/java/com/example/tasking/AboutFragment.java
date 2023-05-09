29d813c09f2749e0a88a2de4d07cc23a971c2f40
63906 <202110256@gordoncollege.edu.ph>
2023-05-10 01:05:11 +0800

    AppDev - with NavBar + Sample Fragments

--- app/src/main/java/com/example/tasking/AboutFragment.java
+++ app/src/main/java/com/example/tasking/AboutFragment.java
@@ -1,7 +1,6 @@
 package com.example.tasking;
 
 import android.os.Bundle;
-import android.sax.Element;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.appcompat.app.AppCompatActivity;
+
 
 public class AboutFragment extends Fragment {
 

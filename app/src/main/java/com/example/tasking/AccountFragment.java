c8f6c1526535c55fc0618c97233899beebde7ac1
63906 <202110256@gordoncollege.edu.ph>
2023-05-14 20:44:57 +0800

    Themese/Alarm na lang no content

--- app/src/main/java/com/example/tasking/AccountFragment.java
+++ app/src/main/java/com/example/tasking/AccountFragment.java
@@ -2,6 +2,9 @@ package com.example.tasking;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,5 +18,33 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_account, container, false);
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_items, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.deleteAccount) {
+            //Delete Account function
+            return true;
+        } else if (id == R.id.deleteData) {
+            //Delete Data function
+            return true;
+        }
+
+
+        return false;
     }
 }
+
